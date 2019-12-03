bcfbdbf2ac9ade915675b19f6a236b6de03912d0
vtsartas <vtsartas@gmail.com>
2019-12-03 19:49:12 +0100

    Mejorado el ejercicio del perímetro para que pregunte si queremos calcular otro.

--- src/tema2/PerimetroCircunferencia.java
+++ src/tema2/PerimetroCircunferencia.java
@@ -4,13 +4,21 @@ import java.util.Scanner;
 
 public class PerimetroCircunferencia {
     public static void main(String[] args) {
+        
         final double PI=3.1415926536;
         double perimetro,radio;
-        System.out.println("Introduce el radio de la circunferencia");
+        String leerotro="s";
         Scanner valor=new Scanner(System.in);
-        radio=valor.nextDouble();
-        perimetro=radio*PI*2;
-        System.out.printf("El perímetro de la circunferencia de radio %.2f es %.4f.",radio,perimetro);
+
+        while (leerotro.equals("s")) {
+            System.out.println("Introduce el radio de la circunferencia");  
+            radio=valor.nextDouble();
+            perimetro=radio*PI*2;
+            System.out.printf("El perímetro de la circunferencia de radio %.2f es %.4f.\n",radio,perimetro);
+            
+            System.out.println("¿Deseas calcular otro perímetro en base a otro radio?(s/n)");
+            leerotro=valor.next();
+        }
         valor.close();
     }
 
