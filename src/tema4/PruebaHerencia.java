79136e59f607c885689598e8a1ae5bdc15a7863d
vtsartas <vtsartas@gmail.com>
2019-12-12 22:39:17 +0100

    Solucionado problema de PruebaHerencia.java

--- src/tema4/PruebaHerencia.java
+++ src/tema4/PruebaHerencia.java
@@ -1,18 +1,19 @@
 package tema4;
 
 public class PruebaHerencia {
+public static void main(String[] args) {
+    
 
     // creación de instancias de la superclase Vehiculot4 y de las subclases TurismoT4, DeportivoT4 y FurgonetaT4
 
-    VehiculoT4 miVehiculo = new VehiculoT4("4050 ABJ","VW", "GTI","Blanco",100.0);
-    TurismoT4 miTurismo = new TurismoT4("4060 TUR","Skoda", "Fabia","Blanco",90.0,2,false);
-    DeportivoT4 miDeportivo = new DeportivoT4("4070 DEP","Ford","Mustang","Rojo",150.0,2000);
-    FurgonetaT4 miFurgoneta = new FurgonetaT4("4080 FUR","Fiat", "Ducato","Azul",80.0,1200,8);
+    VehiculoT4 miVehiculo = new VehiculoT4("4050 ABJ","VW","GTI","Blanco",100);
+    TurismoT4 miTurismo = new TurismoT4("4060 TUR","Skoda","Fabia","Blanco",90,2,false);
+    DeportivoT4 miDeportivo = new DeportivoT4("4070 DEP","Ford","Mustang","Rojo",150,2000);
+    FurgonetaT4 miFurgoneta = new FurgonetaT4("4080 FUR","Fiat", "Ducato","Azul",80,1200,8);
 
-    // invocación del método getAtributos() de cada objeto
+    // invocación del método getAtributosT4() de cada objeto
 
-    System.out.println(miVehiculo.getAtributos());
-    System.out.println("Vehículo : " + miVehiculo.getAtributosT4());
+    System.out.println("Vehículo : "+miVehiculo.getAtributosT4());
 
     // miVehiculo es una instancia de la clase VehiculoT4, se
     // invoca el método getAtributos() de VehiculoT4
@@ -37,5 +38,6 @@ public class PruebaHerencia {
     // superclase Vehiculo: getMatricula(), getMarca() y getModelo()
 
     System.out.println("Turismo : " +miTurismo.getMatricula()+" "+miTurismo.getMarca()+" "+miTurismo.getModelo());
-
+    
+    }
 }
